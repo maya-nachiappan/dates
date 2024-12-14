096bd74c0d4621777630e418d69c9bc59008fe1d
maya-nachiappan <maya.n102304@gmail.com>
2024-12-13 17:03:32 -0800

    Initial Commit

---
+++ dates/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dates
+//
+//  Created by maya nachiappan on 12/13/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
